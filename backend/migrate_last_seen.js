60be2c921cca2530318f79a2811beb018d7a7c0e
dazaran <dazaran52@gmail.com>
2026-08-08 03:04:34 +0200

    chore: remove hardcoded postgres password

--- backend/migrate_last_seen.js
+++ backend/migrate_last_seen.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 const client = new Client({
-  connectionString: 'postgresql://postgres.swprcstdyskalatuvbqh:dazaran!TattooHUB2026@aws-1-eu-west-2.pooler.supabase.com:5432/postgres'
+  connectionString: process.env.DATABASE_URL
 });
 async function run() {
   await client.connect();
