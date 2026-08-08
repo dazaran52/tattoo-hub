ce4d4f7a986ff5fa978266187b0a460fac48690e
dazaran <dazaran52@gmail.com>
2026-08-08 03:29:33 +0200

    chore: add suppressHydrationWarning to html

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -87,7 +87,7 @@ export default async function RootLayout({
   }
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
